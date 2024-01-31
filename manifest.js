cae4eca13ba17ce0b7e319d73fac92fdb4196299
Chris Moesel <cmoesel@mitre.org>
2024-01-31 14:06:39 -0500

    Improvements to manifest.js
    - Don't assume folder name is FSHOnline-Examples
    - Log old and new manifest summaries
    - Minor reformatting tweaks

--- manifest.js
+++ manifest.js
@@ -3,6 +3,7 @@ const path = require('path');
 const moment = require('moment');
 const lineReaderComponent = require('readline');
 
+const baseExamplesPath = path.join(__dirname, 'Examples');
 const manifestPath = path.join(__dirname, 'index.json');
 
 /***
@@ -33,40 +34,35 @@ async function getMetadata(filePath, fileName) {
 }
 
 /***
- * Takes the local file path of a file and retuns the corresponding Github url suffix
- */
-function generatePath(filePath) {
-  let urlPath = filePath.split('FSHOnline-Examples/Examples/')[1];
-  return urlPath.replace(' ', '%20'); // Spaces appear as '%20' in Github urls
-}
-
-/***
  * Returns an array of manifest objects when provided a directory's filepath
  */
-async function getChildren(baseDirName) {
+async function getChildren(relativeDir = '') {
   let workingArray = [];
 
   // First we'll take in all files in the provided directory
-  const files = fs.readdirSync(baseDirName);
+  const files = fs.readdirSync(path.join(baseExamplesPath, relativeDir));
 
   for (const file of files) {
-    const filePath = path.join(baseDirName, file); // We're appending the child file name to the base directory name
-    let fileStats = fs.statSync(filePath); // This contains directory information
+    const relativeFilePath = path.join(relativeDir, file);
+    const fullFilePath = path.join(baseExamplesPath, relativeFilePath); // We're appending the child file name to the base directory name
+    let fileStats = fs.statSync(fullFilePath); // This contains directory information
 
     if (file.endsWith('fsh')) {
-      const index = workingArray.push({ path: generatePath(filePath) }) - 1;
-      const metaData = await getMetadata(filePath, file);
-      workingArray[index].name = metaData.name;
-      workingArray[index].description = metaData.description;
-      workingArray[index].type = "file";
+      const metaData = await getMetadata(fullFilePath, file);
+      workingArray.push({
+        path: encodeURI(relativeFilePath),
+        ...metaData,
+        type: 'file'
+      });
     } else if (fileStats.isDirectory()) {
-      const index = workingArray.push({ name: file, path: generatePath(filePath) }) - 1;
-      workingArray[index].type = 'category';
-      workingArray[index].children = [];
-      const children = await getChildren(filePath);
-      workingArray[index].children = children;
-      if (workingArray[index].children.length === 0) {
-        workingArray.pop();
+      const children = await getChildren(relativeFilePath);
+      if (children.length) {
+        workingArray.push({
+          name: file,
+          path: encodeURI(relativeFilePath),
+          type: 'category',
+          children
+        });
       }
     }
   }
@@ -74,9 +70,41 @@ async function getChildren(baseDirName) {
 }
 
 async function generateManifest() {
-  const examplesArr = await getChildren(path.join(__dirname, 'Examples'));
-  let manifestObj = {timestamp: moment(Date.now()).format(), children: examplesArr };
-  fs.writeFileSync(manifestPath, JSON.stringify(manifestObj, null, "\t"), 'utf-8');
+  let oldManifest;
+  if (fs.existsSync(manifestPath)) {
+    oldManifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
+  } else {
+    oldManifest = { timestamp: 'n/a', children: [] };
+  }
+  console.log('Old Manifest:');
+  console.log('=============')
+  console.log(`${'Timestamp:'.padEnd(15)} ${oldManifest.timestamp}`);
+  oldManifest.children?.forEach(c => {
+    console.log(`${(c.name + ':').padEnd(15)} ${c.children?.length ?? 0} entries`)
+  });
+  console.log();
+
+  const examplesArr = await getChildren();
+  let newManifest = { timestamp: moment(Date.now()).format(), children: examplesArr };
+  console.log('New Manifest:');
+  console.log('=============')
+  console.log(`${'Timestamp:'.padEnd(15)} ${newManifest.timestamp}`);
+  newManifest.children?.forEach(c => {
+    const oldCount = oldManifest.children?.find(oc => oc.name === c.name)?.children?.length ?? 0;
+    const newCount = c.children?.length ?? 0;
+    const delta = newCount - oldCount;
+    const extra = delta !== 0 ? ` (${delta > 0 ? '+': ''}${delta})` : '';
+    console.log(`${(c.name + ':').padEnd(15)} ${newCount} entries${extra}`);
+  });
+  oldManifest.children?.forEach(c => {
+    if (!newManifest.children.some(nc => nc.name == c.name)) {
+      const oldCount = c.children?.length ?? 0;
+      console.log(`${(c.name + ':').padEnd(15)} 0 entries (-${oldCount})`);
+    }
+  });
+  console.log();
+
+  fs.writeFileSync(manifestPath, JSON.stringify(newManifest, null, '\t'), 'utf-8');
 }
 
 generateManifest();
